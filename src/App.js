2197cdadcf70faa02a6c4e9fa1b7a5e9ea8eea31
khushboo <khushboo.kumari@exponentialai.com>
2021-05-03 18:50:15 +0530

    fixed the bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import ViewPage from './component/viewPage';
 import Navbar from './component/navbar';
 
 function App() {
   return (
+    //Use react fragmnet suggested by lalith
     <React.Fragment>
        <Navbar></Navbar>
       <ViewPage></ViewPage>
